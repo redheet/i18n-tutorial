7ce8936b95123e5a2bd962e8f6b3d4b0cdcee2f4
redheet <naufalzidane32@gmail.com>
2022-10-11 03:33:24 +0700

    Done

---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import 'bootstrap/dist/js/bootstrap.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'flag-icon-css/css/flag-icons.min.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
